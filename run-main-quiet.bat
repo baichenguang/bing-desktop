eabbf71502cc40e7acf635b063dc8087609a8836
Bai Chenguang <baichenguang@tengxuntong.com>
2016-11-01 09:16:38 +0800

    增加cd到当前文件所在目录的命令，去掉安装命令

--- run-main-quiet.bat
+++ run-main-quiet.bat
@@ -1,3 +1,4 @@
 @echo off
+cd /d %~dp0
 call setenv.bat
-mvn clean install exec:java -Dexec.mainClass="baichenguang.bing.BingDesktop" -q
+mvn exec:java -Dexec.mainClass="baichenguang.bing.BingDesktop" -q
