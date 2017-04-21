7a0fdee9c1baa437cec84ad2d9ad64c321442300
Bai Chenguang <baichenguang@neutrey.com>
2017-04-21 10:08:04 +0800

    取消clean install

--- run-main.sh
+++ run-main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 _pwd=`pwd`
 cd $(cd "$(dirname "$0")"; pwd)
-mvn clean install exec:java -Dexec.mainClass="baichenguang.bing.BingDesktop"
+mvn exec:java -Dexec.mainClass="baichenguang.bing.BingDesktop"
